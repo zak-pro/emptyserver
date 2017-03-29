2d6a3fefb06b50979c1e70b9cda1aeeafc2bc19a
Sergey_PC <astzak@gmail.com>
2017-03-29 16:42:51 +0300

    res json

--- server.js
+++ server.js
@@ -25,7 +25,14 @@ app.use( express.static( "public" ) );
 
 //setup view to index page
 app.get('/', function (req, res) {
-    res.send("Hello");
+    res.json({
+        _id: 'String',
+        playerName: 'String',
+        level: 'String',
+        expirience: 'String',
+        mass: 'Array',
+        timeSpan: 'String'
+    });
 });
 
 //set environment variables for azure
