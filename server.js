c01c2ae26b7ca7dbda5466d9e16dce20372d6e38
Sergey_PC <astzak@gmail.com>
2017-03-29 16:55:48 +0300

    global

--- server.js
+++ server.js
@@ -8,41 +8,26 @@ var path = require('path');         //define path
 var os = require('os');             //define os
 
 //get ip addresses
-var interfaces = os.networkInterfaces();
-var addresses = [];
-for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
-        var address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
-        }
-    }
-}
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use( express.static( "public" ) );
 
 //setup view to index page
-app.get('/', function (req, res) {
-   /* res.json({
+app.get('/', function (err, res, req) {
+    res.json({
         _id: 'String',
         playerName: 'String',
         level: 'String',
         expirience: 'String',
         mass: 'Array',
         timeSpan: 'String'
-    })*/
+    })
 });
 
-//set environment variables for azure
+
 app.set('port', process.env.PORT || 8080);
 
-//http setup
+
 var server = app.listen(app.get('port'), function () {  //http server listen
 });
-
-//export modules for testing
-module.exports = {
-    addresses: addresses
-};
