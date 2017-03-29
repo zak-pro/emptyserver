34db3a54344ad20bb03631ea8baed8e7b6f16428
Sergey_PC <astzak@gmail.com>
2017-03-29 15:10:14 +0300

    -some updates

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-var express = require('express');
+//main setup
+var express = require('express');   //define express
+var app = express();                //define app with express
+var path = require('path');         //define path
+var os = require('os');             //define os
 
-var app = express();
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
 
 
-app.get('/', function (req, res, err) {
-    res.end("HELLO!");
+
+app.get('/', function (req, res) {
+    res.send("HELLO!");
 });
 
-app.listen(process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
+
+
+var server = app.listen(app.get('port'), function () {  //http server listen
+    var host = server.address().address;
+    var port = server.address().port;
+});
