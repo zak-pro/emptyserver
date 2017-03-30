a021b09b909b81e78943d7bf330c8c15a9004d4e
Sergey_PC <astzak@gmail.com>
2017-03-30 11:16:53 +0300

    -new config

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-//main setup
-var express = require('express');   //define express
-var app = express();                //define app with express
-//main setup
+
 var express = require('express');   //define express
 var app = express();                //define app with express
 var path = require('path');         //define path
-var os = require('os');             //define os
 
-//get ip addresses
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use( express.static( "public" ) );
-
-//setup view to index page
 app.get('/', function (req, res) {
     res.json({
         Витек: 'ПИРОРАС!!!',
@@ -25,9 +15,9 @@ app.get('/', function (req, res) {
     })
 });
 
-
 app.set('port', process.env.PORT || 8080);
 
 
-var server = app.listen(app.get('port'), function () {  //http server listen
+var server = app.listen(app.get('port'), function () {
+    console.log("Server is running on port: " + app.get('port'));
 });
