1902531b24a9546bc133365c7a4293536be60242
Sergey_PC <astzak@gmail.com>
2017-03-30 11:33:46 +0300

    -new config

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+var config = require('config');
 var express = require('express');   //define express
 var app = express();                //define app with express
 var path = require('path');         //define path
@@ -15,9 +15,9 @@ app.get('/', function (req, res) {
     })
 });
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || config.get('port'));
 
 
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(config.get('port'), function () {
     console.log("Server is running on port: " + app.get('port'));
 });
